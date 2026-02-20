fa8477b1d14d422cc684624023d6d62dfd94f050
luis-c2255 <lmodesto63@gmail.com>
2026-02-20 16:51:35 +0100

    up

--- pages/1_🎯 Employee Analytics Dashboard.py
+++ pages/1_🎯 Employee Analytics Dashboard.py
@@ -124,7 +124,7 @@ with st.container():
         nbins=30,
         color='salary', 
         barmode='overlay',
-        pattern_shape='count',
+        pattern_shape='salary',
         title="Satisfaction by Salary Level"
     )
     fig1 = apply_chart_theme(fig1)
