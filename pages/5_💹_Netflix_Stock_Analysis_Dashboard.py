84751cc2629b4c4b5e2d6b1392a3230073571e21
luis-c2255 <lmodesto63@gmail.com>
2026-02-21 17:42:22 +0100

    up

--- pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
+++ pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
@@ -289,6 +289,7 @@ def style_table(df,color_theme):
 
 with col1:
     st.markdown("Top 10 Days with Highest Positive Returns")
+    top_volatile['Date'] = top_volatile['Date'].dt.date
     top_volatile = df.nlargest(10, 'Daily_Return')[['Date', 'Close', 'Daily_Return', 'Volume']]
     top_volatile = top_volatile.sort_values('Date', ascending=True)
 
