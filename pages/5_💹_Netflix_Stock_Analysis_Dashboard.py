f126338daf9c26f10556e3af9fb1e4bf9256f4f2
luis-c2255 <lmodesto63@gmail.com>
2026-02-22 12:59:34 +0100

    up

--- pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
+++ pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
@@ -885,7 +885,7 @@ fig9.add_trace(go.Scatter(
     x=buy_signals['Date'],
     y=buy_signals['Close'],
     mode='markers',
-    marker=dict(color='blue', symbol='triangle-up', size=12, line=dict(width=0)),
+    marker=dict(color='blue', symbol='triangle-up', size=14, line=dict(width=0)),
     name='Buy Signal'
 ))
 
@@ -893,7 +893,7 @@ fig9.add_trace(go.Scatter(
     x=sell_signals['Date'],
     y=sell_signals['Close'],
     mode='markers',
-    marker=dict(color='orange', symbol='triangle-down', size=12, line=dict(width=0)),
+    marker=dict(color='orange', symbol='triangle-down', size=14, line=dict(width=0)),
     name='Sell Signal'
 ))
 
