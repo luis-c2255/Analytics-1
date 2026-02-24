5d03c04460dc8c9025639515222b73b264335ed5
luis-c2255 <lmodesto63@gmail.com>
2026-02-24 20:01:07 +0100

    add

--- pages/6_📦_Retail_Inventory_Analysis_Dashboard.py
+++ pages/6_📦_Retail_Inventory_Analysis_Dashboard.py
@@ -1093,8 +1093,8 @@ with col3:
         Components.metric_card(
             title="Margin Impact",
             value=f"{margin_impact:+.1f}%",
-            delta="Lower" if margin_impact < 0 else "Higher"
-            card_type=
+            delta=f"{Lower" if margin_impact < 0 else "Higher}",
+            card_type="info"
         ), unsafe_allow_html=True
     )
 
